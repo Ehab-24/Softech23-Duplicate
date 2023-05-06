44e1e5827a69036da4865064cbe9dee8e10e1a04
Ahmad Ashfaq <spoiledwit@gmail.com>
2023-05-06 12:29:02 +0500

    initiated vite with tailwindcss router and zustand

---
+++ Client/src/pages/Ai.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+const Ai = () => {
+  return (
+    <Link to="/">
+<div>Ai</div>
+    </Link>
+  )
+}
+
+export default Ai
---
+++ Client/src/pages/Test.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export default function Test() {
+    return (
+        <Link to="/ai">
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </Link>
+    )
+}

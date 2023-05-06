5433a11dcac586ac257c4b622e12ccb48f2b1743
Mohammad Ajwad Alam <ajwadalam2@gmail.com>
2023-05-07 00:12:50 +0500

    jwt fix

--- Server/models/Admin.js
+++ Server/models/Admin.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AdminSchema = new Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    email: { type: String, required: true, unique: true },
+    hash_password: { type: String, required: true }
 });
+
+AdminSchema.methods.comparePassword = function(password) {
+    return bcrypt.compareSync(password, this.hash_password);
+};
 
 module.exports = mongoose.model('Admin', AdminSchema);

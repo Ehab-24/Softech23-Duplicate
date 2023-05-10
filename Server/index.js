675ff988ac9ab0c9b764993f6e0930aeaad70053
Ahmad Ashfaq <spoiledwit@gmail.com>
2023-05-11 00:29:08 +0500

    added new client url in server cors

--- Server/index.js
+++ Server/index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://pixelspalace.vercel.app'],
   credentials: true
 }));
 
